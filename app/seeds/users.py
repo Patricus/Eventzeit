42a915fc497f2bcff712d3204c6a015e0ef764ce
jkintner25 <jkintner25@gmail.com>
2022-07-22 14:48:02 -0500

    changed avatar type to string

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', avatar=2, password='password')
+        username='Demo', email='demo@aa.io', avatar='../../images/1.png', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', avatar=3, password='password')
+        username='marnie', email='marnie@aa.io', avatar='../../images/1.png', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', avatar=1, password='password')
+        username='bobbie', email='bobbie@aa.io', avatar='../../images/1.png', password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
