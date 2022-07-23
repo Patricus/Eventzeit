5ee3fd1a49c11aa3dafb37f387fb9f005c4227b0
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-23 10:50:12 -0700

    remove specific event

--- react-app/src/components/Events/Pages/EventDetails.js
+++ react-app/src/components/Events/Pages/EventDetails.js
@@ -1,10 +1,28 @@
-import React from "react";
-import SingleEvent from "../SingleEvent"
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { acquireEvents } from "../../../store/events";
 
 function EventDetailPage() {
+  const dispatch = useDispatch();
+  const { eventId } = useParams();
+  const event = useSelector(state => state.events[eventId]);
+
+  useEffect(() => {
+    dispatch(acquireEvents());
+  }, [dispatch]);
+
   return (
     <main>
-      <SingleEvent />
+      <h1>Event: {event.name}</h1>
+      <img src={`${event.event_image_url}`} alt={event.name} />
+      <h3>When: {event.date}</h3>
+      <h3>What: {event.description}</h3>
+      <h3>
+        Where: {event.street_address} {event.city} {event.state} {event.zip_code}
+      </h3>
+      <h3>Tickets Avilable: {event.tickets_available}</h3>
+      <Link to={`{/events/${event.id}/tickets}`}>Buy Tickets</Link>
     </main>
   );
 }
