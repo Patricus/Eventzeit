59008a58538f78348a74d37da47fe12e24579c62
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-23 08:55:45 -0700

    add events seeders

--- app/seeds/events.py
+++ app/seeds/events.py
@@ -9,7 +9,7 @@ def seed_events():
         user_id=1,
         category="Water",
         name='really cool super big bouncy party',
-        event_image_url='fake-image.jpg',
+        event_image_url='https://img.freepik.com/free-vector/silhouette-crowd-party-people-starburst-background_1048-13832.jpg',
         date=datetime.datetime.now(),
         description="Non-descript",
         price=8,
@@ -20,8 +20,55 @@ def seed_events():
         state="CA",
         zip_code=90715)
 
+    eventoftheages = Event(
+        user_id=2,
+        category="Fire",
+        name='event of the ages',
+        event_image_url='https://img.freepik.com/free-photo/people-having-party-by-beach_53876-26402.jpg',
+        date=datetime.datetime.now(),
+        description="Non-descript",
+        price=0,
+        max_occupancy=10,
+        tickets_available=10,
+        street_address="123 Home Place",
+        city="Long Beach",
+        state="CA",
+        zip_code=90715)
+
+    bumpbappers = Event(
+        user_id=3,
+        category="Air",
+        name='bounce party',
+        event_image_url='https://img.freepik.com/premium-photo/people-concert_31965-3617.jpg',
+        date=datetime.datetime.now(),
+        description="Non-descript",
+        price=4,
+        max_occupancy=5,
+        tickets_available=0,
+        street_address="123 Home Place",
+        city="Long Beach",
+        state="CA",
+        zip_code=90715)
+
+    discodunkers = Event(
+        user_id=1,
+        category="Earth",
+        name='disco dunkers',
+        event_image_url='https://img.freepik.com/free-photo/excited-audience-watching-confetti-fireworks-having-fun-music-festival-night-copy-space_637285-559.jpg',
+        date=datetime.datetime.now(),
+        description="Non-descript",
+        price=12,
+        max_occupancy=50,
+        tickets_available=4,
+        street_address="123 Home Place",
+        city="Long Beach",
+        state="CA",
+        zip_code=90715)
 
     db.session.add(reallycoolsuperbigbouncyparty)
+    db.session.add(eventoftheages)
+    db.session.add(bumpbappers)
+    db.session.add(discodunkers)
 
     db.session.commit()
 
