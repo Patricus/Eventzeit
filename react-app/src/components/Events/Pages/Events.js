67e1067925be3cb4e4f755a0324fe567306db4c7
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-24 15:53:29 -0700

    search text

--- react-app/src/components/Events/Pages/Events.js
+++ react-app/src/components/Events/Pages/Events.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { acquireEvents } from "../../../store/events";
 import EventsCard from "../Elements/EventsCard";
@@ -6,17 +6,62 @@ import EventsCard from "../Elements/EventsCard";
 function Events() {
   const events = Object.values(useSelector(state => state.events));
   const dispatch = useDispatch();
+  const [search, setSearch] = useState("");
+  const categories = [
+    "Sport",
+    "Party",
+    "Concert",
+    "Dinner",
+    "Game",
+    "Seminar",
+    "Conference",
+    "Workshop",
+    "Social",
+    "Class",
+    "Auction",
+    "Gala",
+    "Festival",
+    "Exercise",
+    "Celebration",
+    "Other",
+  ];
 
   useEffect(() => {
     dispatch(acquireEvents());
   }, [dispatch]);
+
   return (
     <main>
       <h1>Events</h1>
+      <div>
+        <div>
+          {categories &&
+            categories.map(category => {
+              return (
+                <span key={category}>
+                  <label htmlFor={category}>{category}</label>
+                  <input type="checkbox" name={category} />
+                </span>
+              );
+            })}
+        </div>
+        <div>
+          <input
+            type="text"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+            placeholder="Search Events"
+          />
+        </div>
+      </div>
       {events &&
-        events.map(event => {
-          return <EventsCard key={event.id} event={event} />;
-        })}
+        events
+          .filter(event => {
+            return event.name.match(new RegExp(search, "i"));
+          })
+          .map(event => {
+            return <EventsCard key={event.id} event={event} />;
+          })}
     </main>
   );
 }
