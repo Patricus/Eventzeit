b89661ea0c26bb09b44976bc19054bc41a1c7581
jkintner25 <jkintner25@gmail.com>
2022-07-25 17:01:32 -0500

    fixed logout from dashboard

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
-import { useHistory } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
-  const history = useHistory()
   const onLogout = async (e) => {
-    await dispatch(logout());
-    history.push("/")
+    await dispatch(logout()).then(()=>{
+      return <Redirect to={'/'} />
+    })
   };
 
   return <button onClick={onLogout}>Logout</button>;
