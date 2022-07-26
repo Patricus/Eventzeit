0a6e0410b3288e84d8c481482c8c21c9d7a11ca8
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-26 13:46:46 -0700

    merge db versions

---
+++ migrations/versions/20220726_134335_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0edab023373c
+Revises: be68757b6551, ad27d94bcfc7
+Create Date: 2022-07-26 13:43:35.095679
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0edab023373c'
+down_revision = ('be68757b6551', 'ad27d94bcfc7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
