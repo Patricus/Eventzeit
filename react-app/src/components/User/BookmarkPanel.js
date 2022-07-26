f048a894355e16ab16da9a6fae1a4aa0f3eb908a
jkintner25 <jkintner25@gmail.com>
2022-07-26 16:08:19 -0500

    merged dev branch

---
+++ react-app/src/components/User/BookmarkPanel.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const BookmarksBar = styled.div`
+display: flex;
+flex-direction: row;
+width: 100%;
+height: 30px;
+`
+
+function BookmarksPanel() {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default BookmarksPanel;
