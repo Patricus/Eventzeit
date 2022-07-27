5a662edff8ef1a431096a61c6c57d5178b9d9ecd
jvos <john.voskuyl@gmail.com>
2022-07-27 11:20:11 -0700

    working on cascade delete

--- app/models/user.py
+++ app/models/user.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     hashed_password = db.Column(db.String(255), nullable=False)
     tickets = db.relationship('Ticket', back_populates='user', cascade='all, delete-orphan')
 
-    events = db.relationship('Event', secondary=user_events, back_populates='users')
+    events = db.relationship('Event', secondary=user_events, back_populates='users', cascade='all, delete-orphan')
 
     @property
     def password(self):
