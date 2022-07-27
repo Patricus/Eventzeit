1bbba89ea0cff62fa1e95ed367cb9baaa37108d8
jvos <john.voskuyl@gmail.com>
2022-07-27 16:58:36 -0700

    relationships for bookmarks set

--- app/models/bookmark.py
+++ app/models/bookmark.py
@@ -4,11 +4,12 @@ class Bookmark(db.Model):
     __tablename__ = 'bookmarks'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id', ondelete="CASCADE"))
     title = db.Column(db.String(50), nullable=False)
 
     user = db.relationship('User', back_populates='bookmarks')
+    event = db.relationship('Event', back_populates='bookmarks')
 
     def to_dict(self):
         return {
