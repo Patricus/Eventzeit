b6ad114848cd6fcd5c1e0be21359384597859a56
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-27 11:09:15 -0700

    fix edit form

--- app/forms/newEvent_form.py
+++ app/forms/newEvent_form.py
@@ -20,3 +20,20 @@ class EventForm(FlaskForm):
     city = StringField('city', validators=[DataRequired()])
     state = StringField('state', validators=[DataRequired()])
     zipCode = IntegerField('zip_code', validators=[DataRequired()])
+
+
+class EditEventForm(FlaskForm):
+    user_id = IntegerField('user_id', validators=[DataRequired()])
+    category = StringField('category', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+    image = StringField(
+        'image')
+    date = DateTimeField('date', validators=[
+                         DataRequired()], format='%Y-%m-%d %H:%M')
+    description = TextAreaField('description')
+    price = FloatField('price')
+    occupancy = IntegerField('max_occupancy', validators=[DataRequired()])
+    street_address = StringField('street_address', validators=[DataRequired()])
+    city = StringField('city', validators=[DataRequired()])
+    state = StringField('state', validators=[DataRequired()])
+    zipCode = IntegerField('zip_code', validators=[DataRequired()])
