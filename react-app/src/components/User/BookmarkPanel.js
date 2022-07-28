c720dbd98f6ad6fc3a02e9d0b083ada7d73b12c0
jvos <john.voskuyl@gmail.com>
2022-07-27 20:48:03 -0700

    fixed bookmarked events title showing when there are no bookmarks

--- react-app/src/components/User/BookmarkPanel.js
+++ react-app/src/components/User/BookmarkPanel.js
@@ -13,12 +13,16 @@ margin: 20px 0;
 function BookmarksPanel({ bookmarks }) {
 
     return (
-        <BookmarksBar>
-            <h3>Bookmarked Events:</h3>
-            {bookmarks.map(bookmark => {
-                return <Link to={`/events/${bookmark.event_id}`} key={bookmark.id}>{bookmark.title}</Link>
-            })}
-        </BookmarksBar>
+        <>
+        { bookmarks.length > 0 &&
+            <BookmarksBar>
+                <h3>Bookmarked Events:</h3>
+                {bookmarks.map(bookmark => {
+                    return <Link to={`/events/${bookmark.event_id}`} key={bookmark.id}>{bookmark.title}</Link>
+                })}
+            </BookmarksBar>
+        }
+        </>
     );
 };
 
