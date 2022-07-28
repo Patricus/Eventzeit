9cbe6b659d6a7e874963fe6863aee9300796cd7d
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-27 17:24:17 -0700

    add more seeders

--- app/seeds/events.py
+++ app/seeds/events.py
@@ -1,10 +1,103 @@
+from secrets import choice
 from app.models import db
 from app.models.event import Event
 import datetime
+import random
 
 
 # Adds a demo user, you can add other users here if you want
 def seed_events():
+    images = [
+        'https://img.freepik.com/free-vector/silhouette-crowd-party-people-starburst-background_1048-13832.jpg',
+        'https://img.freepik.com/free-photo/people-having-party-by-beach_53876-26402.jpg',
+        'https://img.freepik.com/premium-photo/people-concert_31965-3617.jpg',
+        'https://img.freepik.com/free-photo/excited-audience-watching-confetti-fireworks-having-fun-music-festival-night-copy-space_637285-559.jpg',
+        'https://simpleseasonal.com/wp-content/uploads/2018/08/shutterstock_279093950.jpg',
+        'https://i.insider.com/5eac8da748d92c3d275bb2de?width=700',
+        'https://www.adobe.com/content/dam/cc/us/en/creativecloud/photography/discover/concert-photography/thumbnail.jpeg',
+        'https://www.paradiseretreats.com/wp-content/uploads/2021/03/SBSummerSolsticeParade2016_Dancers_PhotobyDanielleMethmann.JPG-scaled.jpg',
+    ]
+
+    categories = [
+        "Sport",
+        "Party",
+        "Concert",
+        "Dinner",
+        "Game",
+        "Seminar",
+        "Conference",
+        "Workshop",
+        "Social",
+        "Class",
+        "Auction",
+        "Gala",
+        "Festival",
+        "Exercise",
+        "Celebration",
+        "Other",
+    ]
+
+    addresses = [
+        {'street_address': "185 S Patterson Ave D",
+            'city': "Santa Barbara", 'state': "CA", 'zip_code': 93111},
+
+        {'street_address': '5722 Calle Real',
+            'city': 'Goleta', 'state': 'CA', 'zip_code': 93117},
+
+        {'street_address': '3891 State St', 'city': 'Santa Barbara',
+            'state': 'CA', 'zip_code': 93105},
+
+        {'street_address': '217 Stearns Wharf', 'city': 'Santa Barbara',
+            'state': 'CA', 'zip_code': 93101},
+
+        {'street_address': '500 Ninos Dr', 'city': 'Santa Barbara',
+            'state': 'CA', 'zip_code': 93103},
+    ]
+
+    name1 = [
+        'Big',
+        'Fancy',
+        'Small',
+        'Fantastic',
+        'Bodacious',
+        'Grand',
+        'Amazing'
+    ]
+
+    name2 = [
+        'Kahuna',
+        'Shindig',
+        'Get Together',
+        'Meeting of the Minds',
+        'Brunch',
+        'Wedding',
+        'Jam',
+        'Party',
+        'Sleep Over',
+        'Camping Trip',
+        'Hangout'
+    ]
+
+    for i in range(100):
+        address = random.choice(addresses)
+
+        event = Event(
+            user_id=i % 3 + 1,
+            category=random.choice(categories),
+            name=f'{random.choice(name1)} {random.choice(name2)}',
+            event_image_url=random.choice(images),
+            date=datetime.datetime.now() + datetime.timedelta(days=i),
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!",
+            price=i,
+            max_occupancy=i*3,
+            tickets_available=i*3,
+            street_address=address['street_address'],
+            city=address['city'],
+            state=address['state'],
+            zip_code=address['zip_code']
+        )
+        db.session.add(event)
+
     reallycoolsuperbigbouncyparty = Event(
         user_id=1,
         category="Party",
@@ -124,7 +217,6 @@ def seed_events():
         city="San Rafael",
         state="CA",
         zip_code=94901)
-
 
     db.session.add(reallycoolsuperbigbouncyparty)
     db.session.add(eventoftheages)
