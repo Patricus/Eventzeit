090062fcf0c7253709d79797e090afe6915917c4
jvos <john.voskuyl@gmail.com>
2022-07-29 12:59:40 -0700

    flex direction set to column for bookmark links

--- react-app/src/components/User/BookmarkPanel.js
+++ react-app/src/components/User/BookmarkPanel.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const BookmarksBar = styled.div`
 display: flex;
-flex-direction: row;
+flex-direction: column;
 width: 100%;
 height: 40px;
 background-color: red;
