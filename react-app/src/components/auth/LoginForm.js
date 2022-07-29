6f112e6530c03238cc55dca5b7413571fc949bd2
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-28 18:19:29 -0700

    modal white background

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -31,36 +31,28 @@ const LoginForm = setShowLogin => {
   }
 
   return (
-    <main>
-      <form onSubmit={onLogin}>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            name="email"
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={updateEmail}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={updatePassword}
-          />
-          <button type="submit">Login</button>
-        </div>
-      </form>
-    </main>
+    <form onSubmit={onLogin}>
+      <div>
+        {errors.map((error, ind) => (
+          <div key={ind}>{error}</div>
+        ))}
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input name="email" type="text" placeholder="Email" value={email} onChange={updateEmail} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={updatePassword}
+        />
+        <button type="submit">Login</button>
+      </div>
+    </form>
   );
 };
 
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,181 +1,126 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { signUp } from "../../store/session";
+import styled from "styled-components";
 
-import avatar1 from '../../images/1.png'
-import avatar2 from '../../images/2.png'
-import avatar3 from '../../images/3.png'
-import avatar4 from '../../images/4.png'
-import avatar5 from '../../images/5.png'
-import avatar6 from '../../images/6.png'
-import avatar7 from '../../images/7.png'
-import avatar8 from '../../images/8.png'
-import avatar9 from '../../images/9.png'
-import avatar10 from '../../images/10.png'
-import avatar11 from '../../images/11.png'
-import avatar12 from '../../images/12.png'
-import avatar13 from '../../images/13.png'
-import avatar14 from '../../images/14.png'
-import avatar15 from '../../images/15.png'
-
+import avatar1 from "../../images/1.png";
+import avatar2 from "../../images/2.png";
+import avatar3 from "../../images/3.png";
+import avatar4 from "../../images/4.png";
+import avatar5 from "../../images/5.png";
+import avatar6 from "../../images/6.png";
+import avatar7 from "../../images/7.png";
+import avatar8 from "../../images/8.png";
+import avatar9 from "../../images/9.png";
+import avatar10 from "../../images/10.png";
+import avatar11 from "../../images/11.png";
+import avatar12 from "../../images/12.png";
+import avatar13 from "../../images/13.png";
+import avatar14 from "../../images/14.png";
+import avatar15 from "../../images/15.png";
 
 const AvatarImg = styled.img`
-width: 50px;
-height: 50px;
-margin: 10px;
-`
+  width: 50px;
+  height: 50px;
+  margin: 10px;
+`;
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [avatar, setAvatar] = useState(avatar1)
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [avatar, setAvatar] = useState(avatar1);
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
   const user = useSelector(state => state.session?.user);
   const dispatch = useDispatch();
 
-  const onSignUp = async (e) => {
+  const onSignUp = async e => {
     e.preventDefault();
     if (password === repeatPassword) {
       const data = await dispatch(signUp(username, email, password, avatar));
       if (data) {
-        setErrors(data)
+        setErrors(data);
       }
     }
   };
 
-  const updateUsername = (e) => {
+  const updateUsername = e => {
     setUsername(e.target.value);
   };
 
-  const updateEmail = (e) => {
+  const updateEmail = e => {
     setEmail(e.target.value);
   };
 
-  const updatePassword = (e) => {
+  const updatePassword = e => {
     setPassword(e.target.value);
   };
 
-  const updateRepeatPassword = (e) => {
+  const updateRepeatPassword = e => {
     setRepeatPassword(e.target.value);
   };
 
-  const updateAvatar = (e) => {
+  const updateAvatar = e => {
     setAvatar(e.target.value);
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   return (
-    <main>
-
-      <form onSubmit={onSignUp}>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div>
-          <label>User Name</label>
-          <input
-            type='text'
-            name='username'
-            onChange={updateUsername}
-            value={username}
-          ></input>
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type='text'
-            name='email'
-            onChange={updateEmail}
-            value={email}
-          ></input>
-        </div>
-        <div>
-          <label>Avatar</label>
-          <select
-            type='text'
-            name='email'
-            onChange={updateAvatar}
-            value={avatar}
-          >
-            <option value={avatar1}>
-              Blue Hat
-            </option>
-            <option value={avatar2}>
-              Orange Hat
-            </option>
-            <option value={avatar3}>
-              Kitty
-            </option>
-            <option value={avatar4}>
-              Fox
-            </option>
-            <option value={avatar5}>
-              Bald Man
-            </option>
-            <option value={avatar6}>
-              Grey Hair Lady
-            </option>
-            <option value={avatar7}>
-              Grey Hair Lady 2
-            </option>
-            <option value={avatar8}>
-              Missing Eyes
-            </option>
-            <option value={avatar9}>
-              Orange Beard
-            </option>
-            <option value={avatar10}>
-              Kid Boy
-            </option>
-            <option value={avatar11}>
-              Short Hair
-            </option>
-            <option value={avatar12}>
-              Glasses
-            </option>
-            <option value={avatar13}>
-              Long Red Hair
-            </option>
-            <option value={avatar14}>
-              Long Blonde Braid
-            </option>
-            <option value={avatar15}>
-              Kid Girl
-            </option>
-          </select>
-          <AvatarImg src={avatar} />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type='password'
-            name='password'
-            onChange={updatePassword}
-            value={password}
-          ></input>
-        </div>
-        <div>
-          <label>Repeat Password</label>
-          <input
-            type='password'
-            name='repeat_password'
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            required={true}
-          ></input>
-        </div>
-        <button type='submit'>Sign Up</button>
-      </form>
-    </main>
+    <form onSubmit={onSignUp}>
+      <div>
+        {errors.map((error, ind) => (
+          <div key={ind}>{error}</div>
+        ))}
+      </div>
+      <div>
+        <label>User Name</label>
+        <input type="text" name="username" onChange={updateUsername} value={username}></input>
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="text" name="email" onChange={updateEmail} value={email}></input>
+      </div>
+      <div>
+        <label>Avatar</label>
+        <select type="text" name="email" onChange={updateAvatar} value={avatar}>
+          <option value={avatar1}>Blue Hat</option>
+          <option value={avatar2}>Orange Hat</option>
+          <option value={avatar3}>Kitty</option>
+          <option value={avatar4}>Fox</option>
+          <option value={avatar5}>Bald Man</option>
+          <option value={avatar6}>Grey Hair Lady</option>
+          <option value={avatar7}>Grey Hair Lady 2</option>
+          <option value={avatar8}>Missing Eyes</option>
+          <option value={avatar9}>Orange Beard</option>
+          <option value={avatar10}>Kid Boy</option>
+          <option value={avatar11}>Short Hair</option>
+          <option value={avatar12}>Glasses</option>
+          <option value={avatar13}>Long Red Hair</option>
+          <option value={avatar14}>Long Blonde Braid</option>
+          <option value={avatar15}>Kid Girl</option>
+        </select>
+        <AvatarImg src={avatar} />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" onChange={updatePassword} value={password}></input>
+      </div>
+      <div>
+        <label>Repeat Password</label>
+        <input
+          type="password"
+          name="repeat_password"
+          onChange={updateRepeatPassword}
+          value={repeatPassword}
+          required={true}></input>
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
   );
 };
 
