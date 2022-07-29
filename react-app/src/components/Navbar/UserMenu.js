b70d1bf776ee73ed93e95d9afe90535d74189b0b
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-29 10:55:05 -0700

    refactor dropdown

--- react-app/src/components/Navbar/UserMenu.js
+++ react-app/src/components/Navbar/UserMenu.js
@@ -1,80 +1,89 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import LogoutButton from "../auth/LogoutButton";
 import "./navBar.css";
 
 const DropDownMenu = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: absolute;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    box-shadow: 0 0 4px 2px black;
+    border-radius: 4px;
+    background-color: white;
+    width: 100px;
+    top: 8px;
+    left: 3px;
+    padding: 5px;
+    gap: 3px;
 `;
 
 const MenuButton = styled.div`
-  cursor: pointer;
+    cursor: pointer;
 `;
 
 function UserMenu({ user }) {
-  const [showMenu, setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
-  const openMenu = () => {
-    setShowMenu(!showMenu);
-  };
+    const openMenu = e => {
+        if (!showMenu) return setShowMenu(true);
+        if (e.target.classList.contains("dropdown")) return;
+        setShowMenu(false);
+    };
 
-  return (
-    <div className="navlink" style={{ textDecoration: "none" }}>
-      {!showMenu && (
-        <MenuButton onClick={openMenu}>
-          <div className="profile-container" style={{ width: "150px" }}>
-            <div className="user-profile-container">
-              <div
-                className="profile-icon"
-                style={{
-                  backgroundImage: `url(${user.avatar})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "none",
-                  height: "24px",
-                  width: "24px",
-                  marginRight: "0px",
-                  float: "left",
-                }}
-              ></div>
-            </div>
-            <div className="navlink" style={{ paddingTop: "7px" }}>
-              Hi, {user.username} !
-            </div>
-          </div>
-        </MenuButton>
-      )}
-      {showMenu && (
-        <div>
-          <MenuButton onClick={openMenu}>
-            {/* <div className="profile-container" style={{ width: "130px" }}>
-              <div className="profile-icon"></div> */}
-              <p className="navlink" style={{ paddingTop: "4px" }}>
-                Hi, {user.username} !
-              </p>
-            {/* </div> */}
-          </MenuButton>
-          <DropDownMenu >
-            <div id="dropdown" className="user-profile-container navlink">
-              <NavLink
-                to={`/dashboard/${user.id}`}
-                onClick={openMenu}
-                style={{ textDecoration: "none" }}
-              >
-                <p className="navlink">My Dashboard</p>
-              </NavLink>
-              <p style={{ fontFamily: "Eina" }}>{user.username}</p>
-              <p style={{ fontFamily: "Eina" }}>{user.email}</p>
-              <LogoutButton />
-            </div>
-          </DropDownMenu>
+    useEffect(() => {
+        document.addEventListener("mousedown", openMenu);
+        return () => document.removeEventListener("mousedown", openMenu);
+    }, []);
+
+    return (
+        <div className="navlink" style={{ textDecoration: "none" }}>
+            <MenuButton onClick={openMenu}>
+                <div
+                    className="profile-container"
+                    style={{ width: "fit-content", maxWidth: "150px" }}>
+                    <div className="user-profile-container">
+                        <div
+                            className="profile-icon"
+                            style={{
+                                backgroundImage: `url(${user.avatar})`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                backgroundRepeat: "none",
+                                height: "24px",
+                                width: "24px",
+                                marginRight: "0px",
+                                float: "left",
+                            }}></div>
+                    </div>
+                    <div
+                        className="navlink dropdown"
+                        style={{ paddingTop: "7px", whiteSpace: "nowrap" }}>
+                        Hi, {user.username} !
+                    </div>
+                </div>
+            </MenuButton>
+            {showMenu && (
+                <div style={{ position: "relative" }}>
+                    <DropDownMenu className="dropdown">
+                        <NavLink
+                            to={`/dashboard/${user.id}`}
+                            onClick={openMenu}
+                            style={{ textDecoration: "none" }}>
+                            <p className="navlink">My Dashboard</p>
+                        </NavLink>
+                        <p className="dropdown" style={{ fontFamily: "Eina" }}>
+                            {user.username}
+                        </p>
+                        <p className="dropdown" style={{ fontFamily: "Eina" }}>
+                            {user.email}
+                        </p>
+                        <LogoutButton />
+                    </DropDownMenu>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default UserMenu;
