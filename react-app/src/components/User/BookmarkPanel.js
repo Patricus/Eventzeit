f11144675273442c5c87cad2ac8334d23b6ca1cc
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-29 20:50:12 -0700

    Bookmarked events list style

--- react-app/src/components/User/BookmarkPanel.js
+++ react-app/src/components/User/BookmarkPanel.js
@@ -1,27 +1,33 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const BookmarksBar = styled.div`
-display: flex;
-flex-direction: column;
-width: 100%;
-margin: 20px 0;
-`
+    width: 100%;
+    margin: 20px 0;
+`;
 
 function BookmarksPanel({ bookmarks }) {
-
     return (
         <div className="bookmarks-sidebar">
-            {bookmarks.length > 0 &&
+            {bookmarks.length > 0 && (
                 <BookmarksBar>
-                    <h3>Bookmarked Events:</h3>
-                    {bookmarks.map(bookmark => {
-                        return <Link to={`/events/${bookmark.event_id}`} key={bookmark.id}>{bookmark.title}</Link>
-                    })}
+                    <h3>Bookmarked Events</h3>
+                    <div className="bookmarks">
+                        {bookmarks.map(bookmark => {
+                            return (
+                                <Link
+                                    to={`/events/${bookmark.event_id}`}
+                                    key={bookmark.id}
+                                    className="bookmarkLinks">
+                                    {bookmark.title}
+                                </Link>
+                            );
+                        })}
+                    </div>
                 </BookmarksBar>
-            }
+            )}
         </div>
     );
-};
+}
 
 export default BookmarksPanel;
