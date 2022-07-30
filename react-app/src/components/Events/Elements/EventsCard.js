22272400070d1e4643fad35aefa5dcfb17691ebf
elias <99310710+bo-codes@users.noreply.github.com>
2022-07-30 00:53:16 -0700

    eventcard 90%

--- react-app/src/components/Events/Elements/EventsCard.js
+++ react-app/src/components/Events/Elements/EventsCard.js
@@ -7,129 +7,42 @@ function EventsCard({ event }) {
   const [showHiddenText, setShowHiddenText] = useState(false);
 
   return (
-    <div
-      className="main"
-      style={
-        {
-          // height: "300px"
-        }
-      }
-    >
-      <div
-        className="eventCard"
-        style={{
-          "--imageUrl": "url(" + `${event.event_image_url}` + ")",
-          // height: "300px"
-        }}
-      >
-        <Link
-          className="main-link"
-          to={`/events/${event.id}`}
-          style={{
-            textDecoration: "none",
-            color: "#191923",
-            // height: "300px"
-          }}
-        >
-          <div
-            className="info"
-            style={{
-              "--imageUrl": "url(" + `${event.event_image_url}` + ")",
-            }}
-          >
+    <Link className="card" to={`/events/${event.id}`}>
+      <img src={event.event_image_url} className="card-image" alt="" />
+      <div className="card-overlay">
+        <div className="card-header">
+          <svg className="card-arc" xmlns="http://www.w3.org/2000/svg">
+            <path />
+          </svg>
+          <div className="card-header-text">
+            {/* NAME START */}
             {event.name.length > 24 && (
-              <div className="title">
-                <h2
-                  style={{
-                    color: "#191923",
-                  }}
-                >
-                  {event.name.slice(0, 24)} . . .
-                </h2>
-              </div>
+              <h2 className="card-title">{event.name.slice(0, 24)} . . .</h2>
             )}
             {event.name.length <= 24 && (
-              <div className="title">
-                <h2
-                  style={{
-                    color: "#191923",
-                  }}
-                >
-                  {event.name}
-                </h2>
-              </div>
+              <h2 className="card-title">{event.name}</h2>
             )}
-            <div className="category-date">
-              <div
-                style={{
-                  color: "#FF521B",
-                }}
-              >
-                {event.category}
-              </div>
-              <div
-                style={{
-                  color: "#7E7F9A",
-                }}
-              >
-                <span>
-                  {event.date.split(" G")[0].split(":").splice(0, 2).join(":")}
-                </span>
-              </div>
-              {event.description.length > 74 && (
-                <div
-                  className="description-text"
-                  style={{
-                    width: "90%",
-                  }}
-                >
-                  <h3>
-                    {event.description.slice(0, 74)}
-                    {showHiddenText && (
-                      <span className="more-text">
-                        {event.description.slice(500)}
-                      </span>
-                    )}
-                    {!showHiddenText && <span className="dots">...</span>}
-                    {showHiddenText && (
-                      <span>
-                        <button
-                          onClick={() => setShowHiddenText(!showHiddenText)}
-                          style={{
-                            fontFamily: "Eina-semibold",
-                            color: "#FF521B",
-                            background: "none",
-                            marginLeft: "14px",
-                            border: "none",
-                            fontSize: "18px",
-                          }}
-                        >
-                          Less
-                        </button>
-                      </span>
-                    )}
-                  </h3>
-                </div>
-              )}
-              {/* IF DESCRIPTION IS LESS THAN 500 CHARS */}
-              {event.description.length <= 500 && (
-                <h3
-                  className="description-text"
-                  style={{
-                    width: "90%",
-                  }}
-                >
-                  {event.description}
-                </h3>
-              )}
-            </div>
-            {/* <div>
-              <img src={event.event_image_url} alt={event.name} />
-            </div> */}
+            {/* NAME END */}
+            {/*  */}
+            {/* EVENT DATE START */}
+            <span className="card-status">
+              {event.date.split(" G")[0].split(":").splice(0, 2).join(":")}
+            </span>
+            {/* EVENT DATE END */}
           </div>
-        </Link>
+        </div>
+        {event.description.length > 74 && (
+          <p className="card-description">
+            {event.description.slice(0, 74)}
+            <span className="dots">...</span>
+          </p>
+        )}
+        {/* IF DESCRIPTION IS LESS THAN 500 CHARS */}
+        {event.description.length <= 74 && (
+          <p className="card-description">{event.description}</p>
+        )}
       </div>
-    </div>
+    </Link>
   );
 }
 
--- react-app/src/components/User/MyEventsPanel.js
+++ react-app/src/components/User/MyEventsPanel.js
@@ -2,24 +2,35 @@ import { useSelector } from "react-redux";
 import EventsCard from "../Events/Elements/EventsCard";
 
 function MyEventsPanel() {
-    const user = useSelector(state => state.session.user);
-    const userEventsState = useSelector(state => state.events);
-    const userEvents = Object.values(userEventsState).filter(event => event.user_id === user.id);
+  const user = useSelector((state) => state.session.user);
+  const userEventsState = useSelector((state) => state.events);
+  const userEvents = Object.values(userEventsState).filter(
+    (event) => event.user_id === user.id
+  );
 
-    return (
-        <div className="my-event-panel">
-            <h2 style={{ padding: "6px" }}>Your Events</h2>
-            <div className="eventsHolder">
-                {userEvents.length ? (
-                    userEvents.map(event => {
-                        return <EventsCard key={event.id} event={event} />;
-                    })
-                ) : (
-                    <h2>You Don't have any events, yet!</h2>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className="my-event-panel">
+      <h2 style={{ padding: "6px" }}>Your Events</h2>
+      <div className="eventsHolder">
+        {userEvents.length ? (
+          userEvents.map((event) => {
+            return (
+              <div
+                style={{
+                  height: "400px",
+                  width: "400px",
+                }}
+              >
+                <EventsCard key={event.id} event={event} />
+              </div>
+            );
+          })
+        ) : (
+          <h2>You Don't have any events, yet!</h2>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default MyEventsPanel;
