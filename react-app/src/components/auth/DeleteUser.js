23ce86b4b45e9a2ce957bfaa31a8ed1137ee00bb
jvos <john.voskuyl@gmail.com>
2022-07-30 16:52:19 -0700

    delete user modal done

--- react-app/src/components/auth/DeleteUser.js
+++ react-app/src/components/auth/DeleteUser.js
@@ -5,33 +5,39 @@ import { destroyUser } from "../../store/session";
 import { logout } from "../../store/session";
 
 const DeleteUserModal = ({ setShowConfirmDeleteModal }) => {
-    const [demoError, setDemoError] = useState(false);
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const user = useSelector(state => state.session.user);
+  const [demoError, setDemoError] = useState(false);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const user = useSelector((state) => state.session.user);
 
-    const cancelDelete = () => {
-        setShowConfirmDeleteModal(false);
-    };
+  const cancelDelete = () => {
+    setShowConfirmDeleteModal(false);
+  };
 
-    const deleteUser = () => {
-        if (user.id === 1) {
-            setDemoError(true);
-            return;
-        }
-        dispatch(destroyUser(user.id));
-        dispatch(logout());
-        history.push("/events");
-    };
+  const deleteUser = () => {
+    if (user.id === 1) {
+      setDemoError(true);
+      return;
+    }
+    dispatch(destroyUser(user.id));
+    dispatch(logout());
+    history.push("/events");
+  };
 
-    return (
-        <main>
-            {demoError && <p>Can't delete Demo User!</p>}
-            <h2>Are you sure you want to delete your account?</h2>
-            <button onClick={cancelDelete}>Cancel</button>
-            <button onClick={deleteUser}>Delete Account</button>
-        </main>
-    );
+  return (
+    <main>
+      <div className="modal-items-container">
+        {demoError && <p>Can't delete Demo User</p>}
+        <h2 id="delete-user-msg">
+          Are you sure you want to delete your account?
+        </h2>
+        <div className="modal-buttons">
+          <button onClick={cancelDelete}>Cancel</button>
+          <button onClick={deleteUser}>Delete Account</button>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default DeleteUserModal;
