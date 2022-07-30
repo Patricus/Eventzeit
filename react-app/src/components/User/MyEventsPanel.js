8544668084aa1efd8d89e611ea1e2116a53b7d4c
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-30 08:14:58 -0700

    dashboard style

--- react-app/src/components/User/MyEventsPanel.js
+++ react-app/src/components/User/MyEventsPanel.js
@@ -2,42 +2,46 @@ import { useSelector } from "react-redux";
 import EventsCard from "../Events/Elements/EventsCard";
 
 function MyEventsPanel() {
-  const user = useSelector((state) => state.session.user);
-  const userEventsState = useSelector((state) => state.events);
-  const userEvents = Object.values(userEventsState).filter(
-    (event) => event.user_id === user.id
-  );
+    const user = useSelector(state => state.session.user);
+    const userEventsState = useSelector(state => state.events);
+    const userEvents = Object.values(userEventsState).filter(event => event.user_id === user.id);
 
-  return (
-    <div className="my-event-panel">
-      <h2 style={{ padding: "6px", marginLeft: "25px", fontFamily: "Eina-semibold", fontSize: "40px" }}>{user.username}'s' Events</h2>
-      <div
-        className="eventsHolder"
-        style={{
-            marginLeft: "20px"
-        //   justifyContent: "center",
-        }}
-      >
-        {userEvents.length ? (
-          userEvents.map((event) => {
-            return (
-              <div
+    return (
+        <div className="my-event-panel">
+            <h2
                 style={{
-                  height: "333px",
-                  width: "333px",
-                  margin: "11px",
-                }}
-              >
-                <EventsCard key={event.id} event={event} />
-              </div>
-            );
-          })
-        ) : (
-          <h2>You Don't have any events, yet!</h2>
-        )}
-      </div>
-    </div>
-  );
+                    padding: "6px",
+                    marginLeft: "40vw",
+                    fontFamily: "Eina-semibold",
+                    fontSize: "40px",
+                }}>
+                Your Events
+            </h2>
+            <div
+                className="eventsHolder"
+                style={{
+                    marginLeft: "20px",
+                    //   justifyContent: "center",
+                }}>
+                {userEvents.length ? (
+                    userEvents.map(event => {
+                        return (
+                            <div
+                                style={{
+                                    height: "333px",
+                                    width: "333px",
+                                    margin: "11px",
+                                }}>
+                                <EventsCard key={event.id} event={event} />
+                            </div>
+                        );
+                    })
+                ) : (
+                    <h2>You Don't have any events, yet!</h2>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default MyEventsPanel;
--- react-app/src/components/User/TicketPanel.js
+++ react-app/src/components/User/TicketPanel.js
@@ -4,7 +4,7 @@ import Ticket from "../Tickets/Ticket";
 const TicketsDiv = styled.div`
     width: 100%;
     overflow: hidden;
-    height: 160px;
+    height: 180px;
     margin: 10px 0 10px 10px;
 `;
 
@@ -16,6 +16,15 @@ function TicketPanel({ tickets }) {
     return (
         <div className="tickets-panel">
             <>
+                <h2
+                    style={{
+                        padding: "6px",
+                        marginLeft: "25vw",
+                        fontFamily: "Eina-semibold",
+                        fontSize: "40px",
+                    }}>
+                    Your Tickets
+                </h2>
                 {tickets ? (
                     <TicketUL>
                         {tickets.map(ticket => {
