adc430e1f0715afa81db000aa57fcc788c4d7e7c
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2022-07-30 18:39:23 -0700

    Style modals

--- react-app/src/components/Events/Elements/DeleteEventModal.js
+++ react-app/src/components/Events/Elements/DeleteEventModal.js
@@ -3,27 +3,30 @@ import { useHistory } from "react-router-dom";
 import { removeEvent } from "../../../store/events";
 
 const DeleteEventModal = ({ event, setShowConfirmDeleteModal }) => {
-  const dispatch = useDispatch();
-  const history = useHistory();
+    const dispatch = useDispatch();
+    const history = useHistory();
 
-  const cancelDelete = () => {
-    setShowConfirmDeleteModal(false)
-  }
+    const cancelDelete = () => {
+        setShowConfirmDeleteModal(false);
+    };
 
-  const deleteEvent = async (e) => {
-    e.preventDefault();
-    await dispatch(removeEvent(event.id));
-    history.push("/events");
-  };
+    const deleteEvent = async e => {
+        e.preventDefault();
+        await dispatch(removeEvent(event.id));
+        history.push("/events");
+    };
 
-  return (
-    <main>
-      <h2>Are you sure you want to delete your event?</h2>
-      <button onClick={cancelDelete}>Cancel</button>
-      <button onClick={deleteEvent}>Delete Event</button>
-    </main>
-  )
-}
-
+    return (
+        <main>
+            <div className="modal-items-container">
+                <h2 id="delete-user-msg">Are you sure you want to delete your event?</h2>
+                <div className="modal-buttons">
+                    <button onClick={cancelDelete}>Cancel</button>
+                    <button onClick={deleteEvent}>Delete Event</button>
+                </div>
+            </div>
+        </main>
+    );
+};
 
 export default DeleteEventModal;
--- react-app/src/components/auth/EditUserForm.js
+++ react-app/src/components/auth/EditUserForm.js
@@ -21,130 +21,133 @@ import avatar14 from "../../images/14.png";
 import avatar15 from "../../images/15.png";
 
 const AvatarImg = styled.img`
-  width: 50px;
-  height: 50px;
-  margin: 10px;
+    width: 50px;
+    height: 50px;
+    margin: 10px;
 `;
 
 const EditUserForm = ({ setShowUpdateUserModal }) => {
-  const user = useSelector((state) => state.session.user);
-  const dispatch = useDispatch();
+    const user = useSelector(state => state.session.user);
+    const dispatch = useDispatch();
 
-  const [username, setUsername] = useState(user.username || "");
-  const [email, setEmail] = useState(user.email || "");
-  const [avatar, setAvatar] = useState(user.avatar || avatar1);
-  const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+    const [username, setUsername] = useState(user.username || "");
+    const [email, setEmail] = useState(user.email || "");
+    const [avatar, setAvatar] = useState(user.avatar || avatar1);
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+    const [errors, setErrors] = useState([]);
 
-  const updateUser = async (e) => {
-    e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(
-        editUser(user.id, username, email, password, avatar)
-      );
-      if (data) {
-        setErrors(data);
-      }
-    }
-    if (errors.length === 0) setShowUpdateUserModal(false);
-  };
+    const updateUser = async e => {
+        e.preventDefault();
+        if (password === repeatPassword) {
+            const data = await dispatch(editUser(user.id, username, email, password, avatar));
+            if (data) {
+                setErrors(data);
+            } else {
+                setShowUpdateUserModal(false);
+            }
+        }
+    };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
+    const updateUsername = e => {
+        setUsername(e.target.value);
+    };
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
+    const updateEmail = e => {
+        setEmail(e.target.value);
+    };
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
+    const updatePassword = e => {
+        setPassword(e.target.value);
+    };
 
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
+    const updateRepeatPassword = e => {
+        setRepeatPassword(e.target.value);
+    };
 
-  const updateAvatar = (e) => {
-    setAvatar(e.target.value);
-  };
+    const updateAvatar = e => {
+        setAvatar(e.target.value);
+    };
 
-  return (
-    <main>
-      <form onSubmit={updateUser}>
-        <div>
-          {errors &&
-            Array.isArray(errors) &&
-            errors.map((error, idx) => <div key={idx}>{error}</div>)}
-        </div>
-        <div>
-          <label>User Name</label>
-          <input
-            type="text"
-            name="username"
-            onChange={updateUsername}
-            value={username}
-          ></input>
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="text"
-            name="email"
-            onChange={updateEmail}
-            value={email}
-          ></input>
-        </div>
-        <div>
-          <label>Avatar</label>
-          <select
-            type="text"
-            name="email"
-            onChange={updateAvatar}
-            value={avatar}
-          >
-            <option value={avatar1}>Blue Hat</option>
-            <option value={avatar2}>Orange Hat</option>
-            <option value={avatar3}>Kitty</option>
-            <option value={avatar4}>Fox</option>
-            <option value={avatar5}>Bald Man</option>
-            <option value={avatar6}>Grey Hair Lady</option>
-            <option value={avatar7}>Grey Hair Lady 2</option>
-            <option value={avatar8}>Missing Eyes</option>
-            <option value={avatar9}>Orange Beard</option>
-            <option value={avatar10}>Kid Boy</option>
-            <option value={avatar11}>Short Hair</option>
-            <option value={avatar12}>Glasses</option>
-            <option value={avatar13}>Long Red Hair</option>
-            <option value={avatar14}>Long Blonde Braid</option>
-            <option value={avatar15}>Kid Girl</option>
-          </select>
-          <AvatarImg src={avatar} />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            onChange={updatePassword}
-            value={password}
-          ></input>
-        </div>
-        <div>
-          <label>Repeat Password</label>
-          <input
-            type="password"
-            name="repeat_password"
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            required={true}
-          ></input>
-        </div>
-        <button type="submit">Update User Account</button>
-      </form>
-    </main>
-  );
+    return (
+        <main>
+            <form onSubmit={updateUser}>
+                <div className="modal-title">
+                    <h2>Update User</h2>
+                </div>
+                {errors.length > 0 && (
+                    <div className="error-container">
+                        <ul>
+                            {errors.map((error, ind) => (
+                                <li key={ind}>{error}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                <div className="modal-items-container">
+                    <div className="modal-items">
+                        <label>User Name</label>
+                        <input
+                            type="text"
+                            name="username"
+                            onChange={updateUsername}
+                            value={username}></input>
+                    </div>
+                    <div className="modal-items">
+                        <label>Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            onChange={updateEmail}
+                            value={email}></input>
+                    </div>
+                    <div className="modal-items">
+                        <label>Avatar</label>
+                        <select type="text" name="email" onChange={updateAvatar} value={avatar}>
+                            <option value={avatar1}>Blue Hat</option>
+                            <option value={avatar2}>Orange Hat</option>
+                            <option value={avatar3}>Kitty</option>
+                            <option value={avatar4}>Fox</option>
+                            <option value={avatar5}>Bald Man</option>
+                            <option value={avatar6}>Grey Hair Lady</option>
+                            <option value={avatar7}>Grey Hair Lady 2</option>
+                            <option value={avatar8}>Missing Eyes</option>
+                            <option value={avatar9}>Orange Beard</option>
+                            <option value={avatar10}>Kid Boy</option>
+                            <option value={avatar11}>Short Hair</option>
+                            <option value={avatar12}>Glasses</option>
+                            <option value={avatar13}>Long Red Hair</option>
+                            <option value={avatar14}>Long Blonde Braid</option>
+                            <option value={avatar15}>Kid Girl</option>
+                        </select>
+                        <AvatarImg id="avatar-img" src={avatar} />
+                    </div>
+                    <div className="modal-items">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            onChange={updatePassword}
+                            value={password}></input>
+                    </div>
+                    <div className="modal-items">
+                        <label>Repeat Password</label>
+                        <input
+                            type="password"
+                            name="repeat_password"
+                            onChange={updateRepeatPassword}
+                            value={repeatPassword}
+                            required={true}></input>
+                    </div>
+                    <div className="modal-items">
+                        <button id="sign-up" type="submit">
+                            Update User
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </main>
+    );
 };
 
 export default EditUserForm;
